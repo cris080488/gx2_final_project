3e05be0d1940a4a3a538d453609cb6991d4e36a1
criosilva <crisosilva@gmail.com>
2022-08-03 01:40:47 -0300

    add modals functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var global_user = ''
 var page_history = ''
-let data;
+var data;
+
+/* Load Data*/
 
 fetch('./data/data.json').then(response=>{
     return response.json()
@@ -8,9 +10,13 @@ fetch('./data/data.json').then(response=>{
     data = body.data
 })
 
-/* login */
+
+/* back page*/
+
+/* Login */
 
 function login() {
+    
     var user = document.getElementById('login-email').value
     var pwd = document.getElementById('login-pwd').value
     
@@ -27,45 +33,89 @@ function login() {
     }
 }
 
-/* home */
-
+/* Home */
 function openWindow(url) {   
     
     window.location.assign(url)
 }
 
-
-/*document.getElementById("home-library").addEventListener("click", updateLibrary);*/
-
-
 /* Library */
 
 function updateLibrary() {
     
-    var books = data['books']
+    var books = data.books
 
     books.forEach(element => {
         
             let card = document.createElement("div")
             card.className = "book-card"
+            card.name = element.tittle
         
             let photo = document.createElement('img')
             photo.src = element.image
             card.appendChild(photo)
         
-            let name = document.createTextNode(element.tittle);
+            let name = document.createElement('p')
+            name.innerText = element.tittle;
             card.appendChild(name);
         
             let container = document.getElementById("book-galery");
             container.appendChild(card);
         })
+
+    var cards = document.getElementsByClassName('book-card')
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function (e) {
+
+            book = books.find(b => b.tittle === this.children[1].innerHTML)
+            document.getElementById("book-image").src = book.image
+            document.getElementById("book-title").innerHTML = book.tittle
+            document.getElementById("book-synopse").innerHTML = book.synopsis
+            document.getElementById("book-author").innerHTML = book.author
+            document.getElementById("book-gender").innerHTML = book.genre
+            document.getElementById("book-date").innerHTML = book.systemEntryDate
+
+            let modal = document.getElementById("book-modal");
+            modal.style.display = "block" ;
+
+        })
+    }
 }
 
-/* historic */
+/* Modals */
 
-function historic() {
+document.getElementById("button-rent").addEventListener("click", function() {
+    let book_modal = document.getElementById("book-modal");
+    let rent_modal = document.getElementById("rent-modal");
+    rent_modal.style.display = "block" ;
+    book_modal.style.display = "none" ;
+})
 
-    var books = data['books']
+document.getElementById("btn-edit").addEventListener("click", function() {
+    let book_modal = document.getElementById("book-modal");
+    book_modal.style.display = "none" ;
+    openWindow('edit.html')
+})
+
+document.getElementById("btn-disable").addEventListener("click", function() {
+    let book_modal = document.getElementById("book-modal");
+    let dis_modal = document.getElementById("disable-modal");
+    dis_modal.style.display = "block" ;
+    book_modal.style.display = "none" ;
+})
+
+document.getElementById("btn-hist").addEventListener("click", function() {
+    let book_modal = document.getElementById("book-modal");
+    let hist_modal = document.getElementById("hist-modal");
+    hist_modal.style.display = "block" ;
+    book_modal.style.display = "none" ;
+})
+
+/* Historic */
+
+function history() {
+
+    var books = data.books
     var i = 0
     let tb_body = document.getElementById('bd-historic-table')
 
@@ -87,10 +137,6 @@ function historic() {
             cell3.innerHTML = element.tittle
             cell4.innerHTML = hist.withdrawalDate
             cell5.innerHTML = hist.deliveryDate
-
-
         }    
-
-
     })
 }
